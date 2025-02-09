e6a2827d2b38e968a36f9b05505505e7d24db0d9
Jireh <jirehtumbagahan@gmail.com>
2025-02-09 16:52:15 +0800

    chore: add mock data for messages

---
+++ src/mock/messages.ts
@@ -0,0 +1,7 @@
+import { ChatI } from "@/lib/types";
+
+export const chatHistory: ChatI[] = [
+  { message: "Hello, how are you?", sender: "John", timeStamp: "12:01 PM" },
+  { message: "Zack has joined the room", sender: "system", timeStamp: "12:04 PM" },
+  { message: "Heyya", sender: "cloud,", timeStamp: "12:05 PM" },
+];
