ed0a05f19d044d30d53d5057a320bd2f61e952b1
Jireh <jirehtumbagahan@gmail.com>
2025-02-09 16:56:47 +0800

    refactor: use form element and add a function to handle form submission

--- src/components/JoinRoom.tsx
+++ src/components/JoinRoom.tsx
@@ -4,26 +4,41 @@ import Input from "./Input";
 
 interface JoinRoomProps {
   onJoinRoom: () => void;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const JoinRoom = ({ onJoinRoom, onChange }: JoinRoomProps) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onJoinRoom();
+  };
+
   return (
     <>
       <header>
         <h1 className="text-4xl text-center">Real Time Chat App</h1>
       </header>
 
-      <div className="flex flex-col space-y-3 ">
-        <Input className="w-full" id="username" placeholder="Enter username" onChange={onChange}/>
-        <Input className="w-full" id="room" placeholder="Enter room" onChange={onChange}/>
+      <form onSubmit={handleSubmit} className="flex flex-col space-y-3 ">
+        <Input
+          className="w-full"
+          id="username"
+          placeholder="Enter username"
+          onChange={onChange}
+        />
+        <Input
+          className="w-full"
+          id="room"
+          placeholder="Enter room"
+          onChange={onChange}
+        />
         <button
           className="p-3 text-white bg-blue-500 hover:bg-blue-600 rounded-md"
           onClick={onJoinRoom}
         >
           Enter
         </button>
-      </div>
+      </form>
 
       <footer className="text-center">
         <p className="text-xs text-neutral-400">Powered by Next.js</p>
